938b7eca5cf39c8d41e300074f7870912322b707
Tarek <tareksaidee@hotmail.com>
2016-05-08 01:01:53 -0400

    Added comments and fixed a bug.

--- cli/Echo.java
+++ cli/Echo.java
@@ -3,17 +3,17 @@ class Echo{
 	private final int E=0, N=1;
 	private int opts[], f;
 	private String str;
-	
+	//no-argument constructor
 	Echo(){
 		opts=new int[]{-1,-1};
 		str=new String();
 		f=0;
 	}
-	
-	private void setOptions(int i){
+	//change the formatting options
+	void setOptions(int i){
 		opts[i]=i;
 	}
-
+	//parse command line input and set the options
 	void parseArgs(String [] args){
 		for(int i=0;i<args.length && f<2;i++){
                         switch(args[i]){
@@ -35,7 +35,7 @@ class Echo{
 		}
 		str=strBuilder.toString();
 	}
-
+	//returns the final string after applying all the options
 	String repeat(){
 		if(opts[E]==E){
 			str=str.replace("\\n","\n");
