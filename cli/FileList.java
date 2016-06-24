d56ac11719bc9b4ba6e8411de38ed5d72a2eea88
Tarek <tareksaidee@hotmail.com>
2016-06-23 21:40:20 -0400

    add a parseArgs method and format code

--- cli/FileList.java
+++ cli/FileList.java
@@ -7,25 +7,41 @@ import java.nio.file.attribute.*;
 import java.lang.*;
 import java.text.*;
 public class FileList{
+
 	private List<File> list;
+    private static String path=new String(".");
 	public final static int  ALL=0, EXTENDED=1, CANONICAL=2;
 	private static int options[]={-1,-1,-1};
 
-	private FileList(List<File> l){				//private constructor
+    //constructor
+	private FileList(List<File> l){
 		list=l;
 	}
 
-	public static void  setOptions(int i){			//records entered options
+    //setter method for options
+	public static void  setOptions(int i){
 		options[i]=i;
 	}
-	
-	public static int [] getOptions(){			//returns the list of options
+
+    //getter method for options
+	public static int [] getOptions(){
 		return options;
 	}
 
-	public void add(File f) throws FileNotFoundException, SecurityException{	//adds a file to object's list
+    //getter method for path
+    public static String getPath(){
+        return path;
+    }
+
+    //setter method for path
+    public static void setPath(String p){
+        path=p;
+    }
+
+    //adds files to the list after verifying that they're valid
+	public void add(File f) throws FileNotFoundException, SecurityException{
 		Path p=f.toPath();
-		File temp=new File(""); 
+		File temp=new File("");
 		boolean isAbs=p.isAbsolute();
                 for(int i=1;i<=p.getNameCount();i++){
                 	String s=p.subpath(0,i).toString();
@@ -40,10 +56,11 @@ public class FileList{
                this.list.add(temp);
 	}
 
-	public static FileList of(String path) throws FileNotFoundException, SecurityException {	//creates a FileList object after
-		Path p=Paths.get(path);									//checking if the path is valid
-		FileList f=FileList.empty();								//and adds the file(s) to the object
-		List<File> lst=new ArrayList<File>();		
+    //verifies that a path is valid and calls the add() method
+	public static FileList of(String path) throws FileNotFoundException, SecurityException {
+		Path p=Paths.get(path);
+		FileList f=FileList.empty();
+		List<File> lst=new ArrayList<File>();
 		File temp=new File("");
 		boolean isAbs=p.isAbsolute();
 		for(int i=1;i<=p.getNameCount();i++){
@@ -63,41 +80,45 @@ public class FileList{
 		return f;
 	}
 
-	public static FileList empty(){						//creates an empty FileList object
+    //static factory method to create an empty FileList object
+	public static FileList empty(){
 		List<File> l=new ArrayList<File>();
 		return new FileList(l);
 	}
 
-	public  List<File> files(){						//returns a copy of an object's list
+    //returns a copy of the files list
+	public  List<File> files(){
 		List<File> l=new ArrayList<File>(this.list);
 		return l;
 	}
 
-	public boolean contains(File f){					//checks if a list contains a certian file
+    //checks if the list contains a certain file
+	public boolean contains(File f){
 		for(File temp:this.list)
 			if(f.equals(temp))
 				return true;
 		return false;
 	}
 
-	public static List<String> format(FileList flist, int ... opts) throws IOException{	//formats the output based on the options
+    //formats the output based on the command line options
+	public static List<String> format(FileList flist, int ... opts) throws IOException{
 		List<String> slist=new ArrayList<String>();
 		String temp=new String();
-		Collections.sort(flist.list);				
+		Collections.sort(flist.list);
 		for(File f: flist.list){
 			if(opts[ALL]==ALL || !f.isHidden()){
 				if(opts[EXTENDED]==EXTENDED){
 					if(f.isDirectory())
 						temp="d";
 					else
-						temp="-";		
+						temp="-";
 				PosixFileAttributes attr=Files.readAttributes((f.toPath()),PosixFileAttributes.class);
 				temp=temp.concat(PosixFilePermissions.toString(attr.permissions())+" ");
 				temp=temp.concat(attr.owner()+"\t");
 				temp=temp.concat(attr.group()+"  ");
 				temp=temp.concat(Long.toString(attr.size())+"  ");
 				temp=temp.concat(attr.lastModifiedTime()+"   ");
-	
+
 				}
 			if(opts[CANONICAL]==CANONICAL)
 				temp=temp.concat(f.getCanonicalPath());
@@ -105,42 +126,44 @@ public class FileList{
 				temp=temp.concat(f.getName());
 			if(f.isDirectory())
 				temp=temp.concat("/");
-					
+
 			slist.add(temp);
 			temp=new String();
 			}
 		}
-			
-		return slist; 
+
+		return slist;
 	}
 
+    //parses the command line arguments
+    public static void parseArgs(String [] args){
+        for(int i=0;i<args.length;i++){
+            if(args[i].charAt(0)=='-'){
+                for(int x=1;x<args[i].length();x++)
+                    switch(args[i].charAt(x)){
+                        case 'A': FileList.setOptions(FileList.ALL);
+                              break;
+                        case 'c': FileList.setOptions(FileList.CANONICAL);
+                              break;
+                        case 'l': FileList.setOptions(FileList.EXTENDED);
+                              break;
+                        default: System.out.println("Invalid options");
+                              System.exit(1);
+                    }
+                }
+            else{
+                path=args[i];
+                break;
+            }
+        }
+    }
 
 
-	
-	public static void main(String [] args) throws FileNotFoundException, IOException{		//command line parsing is done in main
-			String s=new String(".");
+	public static void main(String [] args) throws FileNotFoundException, IOException{
+            FileList.parseArgs(args);
 			FileList fl=FileList.empty();
-			for(int i=0;i<args.length;i++){
-				if(args[i].charAt(0)=='-'){
-					for(int x=1;x<args[i].length();x++)
-						switch(args[i].charAt(x)){
-							case 'A': FileList.setOptions(FileList.ALL);
-								  break;
-							case 'c': FileList.setOptions(FileList.CANONICAL);
-								  break;
-							case 'l': FileList.setOptions(FileList.EXTENDED);
-								  break;
-							default: System.out.println("Invalid options");
-								  System.exit(1);
-						}
-				}
-				else{
-					s=args[i];
-					break;
-				}
-			}
 			try{
-				fl=FileList.of(s);
+				fl=FileList.of(FileList.getPath());
 			}
 			catch(FileNotFoundException e){
 				System.out.println(e.getMessage());
@@ -148,10 +171,10 @@ public class FileList{
 			}
 			catch(SecurityException e){
 				System.out.println(e.getMessage());
-                                System.exit(1);
-                        }
+                System.exit(1);
+            }
 			List<String> mlist=FileList.format(fl,FileList.getOptions());
 			for(String str:mlist)
-				System.out.println(str);				
+				System.out.println(str);
 	}
 }
