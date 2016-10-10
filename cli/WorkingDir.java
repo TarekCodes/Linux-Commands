6f926048786700fe7a6c4eef1ce9b6a50edee605
Tarek <tareksaidee@hotmail.com>
2016-10-10 16:32:50 -0400

    Add PWD command

---
+++ cli/WorkingDir.java
@@ -0,0 +1,16 @@
+package cli;
+import java.io.*;
+
+class WorkingDir {
+
+  static String getCurrentDir() throws IOException {
+    File file = new File(".");
+    return file.getCanonicalPath();
+
+  }
+
+  public static void main(String [] args)throws IOException {
+    System.out.println(getCurrentDir());
+  }
+
+}
